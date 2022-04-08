da93a92bdada51559bf078318041d167d69ccd69
almafrank <92314215+almafrank@users.noreply.github.com>
2022-04-08 09:21:14 +0200

    Pushar nytt

---
+++ ProjectFora/Client/Services/IMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectFora.Client.Services
+{
+    public interface IMessage
+    {
+        Task Post(MessageModel postmessage);
+        Task<MessageModel?> DeleteMessage(int id);
+        Task<List<MessageModel>> GetAllMessages();
+    }
+}
--- ProjectFora/Client/Services/Message.cs
+++ ProjectFora/Client/Services/Message.cs
@@ -1,11 +1,38 @@
-﻿namespace ProjectFora.Client.Services
+﻿using ProjectFora.Shared;
+using System.Net.Http.Json;
+
+namespace ProjectFora.Client.Services
 {
+<<<<<<< Updated upstream
     public interface IMessage
     {
 
     }
     public class Message
+=======
+    public class Message : IMessage
+>>>>>>> Stashed changes
     {
+        private readonly HttpClient _httpClient;
 
+        public Message(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        //ta bort ett meddelande
+        public async Task<MessageModel?> DeleteMessage(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<MessageModel>($"message/deletemessage/{id}");
+        }
+        //hämta meddelanden
+        public async Task<List<MessageModel>> GetAllMessages()
+        {
+            return await _httpClient.GetFromJsonAsync<List<MessageModel>>("message/GetMessages");
+        }
+        //ta bort ett meddelande
+        public async Task Post(MessageModel postmessage)
+        {
+            await _httpClient.PostAsJsonAsync("message/postmessage", postmessage);
+        }
     }
 }
